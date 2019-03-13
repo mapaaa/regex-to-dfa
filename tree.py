171de5836035df754154f7938bd2affcc23e90bf
Maria Pandele <maria.pandele33@gmail.com>
2019-03-14 00:05:25 +0200

    updated class fields

--- tree.py
+++ tree.py
@@ -1,11 +1,24 @@
 class Data:
-    def __init__(self, operator=None, operand=None, label=None):
+    def __init__(self, operator=None, operand=None, label=None, ind=None):
         self.operator = operator
         self.operand = operand
         self.label = label
+        self.ind = ind
+        self.nullable = None
+        self.first_pos = None
+        self.last_pos = None
 
 class Node:
     def __init__(self, left=None, right=None):
         self.left = None
         self.right = None
         self.data = None
+
+    def is_leaf(self):
+        return (self.left == None and self.right == right)
+
+    def get_operand(self):
+        return self.data.operand
+
+    def get_ind(self):
+        return self.data.ind
