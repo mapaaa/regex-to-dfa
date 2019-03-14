dc00305fffe20eb32a0268ecce53872c02d3f57a
Maria Pandele <maria.pandele33@gmail.com>
2019-03-14 22:40:38 +0200

    completed dfa class

--- dfa.py
+++ dfa.py
@@ -4,18 +4,31 @@ class State:
         self.is_final = is_final
         self.pos = pos
 
+    def __str__(self):
+        s = ''
+        s = 'label = ' + str(self.label) + ' is_final = ' + str(self.is_final) + ' pos = ' + str(self.pos)
+        return s
+
     def is_final(self):
         return self.is_final
 
 class Dfa:
-    def __init__(self, Q=None, sigma=None, delta=None, q0=None, F=None):
+    def __init__(self, Q=None, sigma=None, q0=None, F=None):
        self.Q = Q
        self.sigma = sigma
-       self.delta = delta
+       self.delta = {}
        self.q0 = q0
        self.F = F
        self.cnt_nodes = 0
 
     def new_state(self, pos, is_final=False):
         self.cnt_nodes += 1
-        return State(self.cnt_nodes, pos, is_final)
+        state = State(self.cnt_nodes, pos, is_final)
+        self.Q += state
+        return state
+
+    def get_pos(self, p):
+        return self.Q[p].pos
+
+    def add_transition(self, src_state, ch, dst_state):
+        self.delta[src_state] = (ch, dst_state)
